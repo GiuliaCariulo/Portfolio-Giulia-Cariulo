5cbf88d1bf51c38c9be9af8a212bcd6472d59dec
GiuliaCariulo <98399966+GiuliaCariulo@users.noreply.github.com>
2026-01-14 14:30:49 +0100

    menu coling auto

--- src/js/main.js
+++ src/js/main.js
@@ -100,9 +100,11 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 var burger = document.querySelector(".menu-burger");
 var menu = document.querySelector(".menu");
+var menuClose = document.querySelector(".menu-close");
 
 function toggleMenu() {
   menu.classList.toggle("is-active");
   burger.classList.toggle("is-active");
 }
 burger.addEventListener("click", toggleMenu);
+menuClose.addEventListener("click", toggleMenu);
